01e2f3012842777e9fb4a083004effaa6440c61e
marcomg <mmarcogguerrini@gmail.com>
2013-06-01 20:19:44 +0200

    Added sql database code

---
+++ includes/database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `paste` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tid` text COLLATE utf8_bin NOT NULL,
+  `username` text COLLATE utf8_bin NOT NULL,
+  `posted` text COLLATE utf8_bin NOT NULL,
+  `code` longtext COLLATE utf8_bin NOT NULL,
+  `lang` text COLLATE utf8_bin NOT NULL,
+  `expires` text COLLATE utf8_bin NOT NULL,
+  `sha1` text COLLATE utf8_bin NOT NULL,
+  `hidden` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
