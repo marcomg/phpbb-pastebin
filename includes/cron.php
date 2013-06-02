06d77d8394b6ca03eff1f4e597acfc0c83aa0d31
marcomg <mmarcogguerrini@gmail.com>
2013-06-02 15:33:31 +0200

    Remove bug in sql code

--- includes/cron.php
+++ includes/cron.php
@@ -3,7 +3,7 @@
 if($config_enable_cron){
     $_ = file_get_contents(ROOT.'/includes/text_databases/cron.txt');
     if(time() -$_ > $config_cron_time){
-        $db->query('DELETE FROM `paste` WHERE `expires` != -1 and `posted` + `expires` > '.time().';');
+        $db->query('DELETE FROM `paste` WHERE `expires` != -1 and `posted` + `expires` < '.time().';');
         file_put_contents(ROOT.'/includes/text_databases/cron.txt', time());
     }
 }
